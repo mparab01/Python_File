6cbe1e1bbfc13f5f0b8087f896b672eb197578a1
mparab01 <infoeastindia1@gmail.com>
2016-09-30 23:09:40 -0500

    Python_Files
    https://docs.python.org/2/tutorial/inputoutput.html

---
+++ Files.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[12]:
+
+f = open("C:\Users\Om\Documents\GitHub\Python_Files\TestFile.txt", 'r')
+
+
+# In[13]:
+
+f.readlines()
+
+
+# In[16]:
+
+for words in open("C:\Users\Om\Documents\GitHub\Python_Files\TestFile.txt"):
+    print words
+
+
+# In[ ]:
+
+
+
